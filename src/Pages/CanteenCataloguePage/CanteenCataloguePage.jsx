2c7e68872c4d70ca5c0276e3a32e9b41aadf5357
Aryashu-1 <aryashu448@gmail.com>
2024-05-06 23:04:23 +0530

    arrow added

--- src/Pages/CanteenCataloguePage/CanteenCataloguePage.jsx
+++ src/Pages/CanteenCataloguePage/CanteenCataloguePage.jsx
@@ -7,7 +7,15 @@ import Menu from '../../Components/Menu/Menu'
 const CanteenCataloguePage = () => {
   return (
     <div>
-   
+        <div>
+          <img className='h-[35px] w-[35px] pl-2 pt-2' src="https://static.thenounproject.com/png/234369-200.png" />
+        </div>
+        <div className='h-[72px] flex'> 
+            <div className='pl-[34px]'>
+              <img className='' src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg" alt="" />
+            </div>
+            <div></div>
+        </div>
       <ProductCard/>
       <SearchBar/>
       
