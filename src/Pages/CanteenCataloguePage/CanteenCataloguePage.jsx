c3d7def4233bacdd6521e803ce66eaaa939de447
Aryashu-1 <aryashu448@gmail.com>
2024-05-06 21:56:57 +0530

    home page updated

--- src/Pages/CanteenCataloguePage/CanteenCataloguePage.jsx
+++ src/Pages/CanteenCataloguePage/CanteenCataloguePage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ProductCard from '../../Components/ProductCard/ProductCard'
 import SearchBar from '../../Components/SearchBar/SearchBar'
-import Filter from '../../Components/Filter/Filter'
+
 import Menu from '../../Components/Menu/Menu'
 
 const CanteenCataloguePage = () => {
   return (
     <div>
-      CAnteen Catalogue
+   
       <ProductCard/>
       <SearchBar/>
-      <Filter/>
+      
       <Menu/>
     </div>
   )
