e108f01cefbc073d5b412a1759de37ce31b00b3a
Aryashu-1 <aryashu448@gmail.com>
2024-05-09 23:01:48 +0530

    added signin and signup page

--- src/Pages/SigninPage/SigninPage.jsx
+++ src/Pages/SigninPage/SigninPage.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { NavLink, useNavigate } from "react-router-dom";
 
-const SigninPage = () => {
+function Signin() {
+  let {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  let navigate = useNavigate();
+
+let [user,setUser] = useState([])
+
+
+
+  function handleFormSubmit(User) {
+    console.log(User)
+    setUser(User)
+    
+   
+  }
   return (
-    <div>
-      SignIN
+    <div className=" h-screen ">
+      <div className="flex h-screen">
+        <div className="w-[60%] bg-[url('https://images.unsplash.com/photo-1586515779592-d94f096e4c41?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+        
+        </div>
+      
+      
+      <div className="w-[40%] rounded-md m-auto ">
+        <div className="text-center"> <h1 className=" mx-6 my-3 font-semibold text-2xl">Sign In</h1></div>
+        
+        <form className="px-10 m-2" onSubmit={handleSubmit(handleFormSubmit)}>
+          <div className="py-3">
+            {" "}
+            <h1 className="text-xl font-semibold">Username</h1>
+            <input
+              className=" w-full p-2 rounded-md border-2 border-black"
+              type="text"
+              {...register("username", { required: true })}
+            ></input>
+          </div>
+          {errors.Username?.type === "required" && (
+            <h3 className=" text-red-700">*required</h3>
+          )}
+          <div className="py-3">
+            <h1 className="text-xl font-semibold">Password</h1>
+            <input
+              id="Pass"
+              className=" w-full p-2 rounded-md border-2 border-gray-500"
+              type="password"
+              {...register("password", { required: true, minLength: 8 })}
+            ></input>
+            {errors.Pass?.type === "required" && (
+              <h3 className=" text-red-700">*required</h3>
+            )}
+            {errors.Pass?.type === "minLength" && (
+              <h3 className=" text-red-700">
+                Password must be atleat 8 cahracters
+              </h3>
+            )}
+          </div>
+          <div className="text-center p-4">
+           <button
+              type="submit"
+              className=" bg-blue-600 w-[50%] text-lg rounded-xl p-3 text-white hover:bg-blue-700 "
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+        <p className="text-center text-lg py-3">
+          Don't have an account ?
+          <NavLink
+            to="/Signup"
+            className="underline px-3 text-blue-500 font-semibold text-lg"
+          >
+            Sign up
+          </NavLink>
+        </p>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SigninPage
+export default Signin;
--- src/Pages/SignupPage/SignupPage.jsx
+++ src/Pages/SignupPage/SignupPage.jsx
@@ -1,11 +1,82 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import {useNavigate,NavLink} from 'react-router-dom' 
 
-const SignupPage = () => {
+function Signup() {
+    let {register, handleSubmit,formState:{errors}}= useForm();
+    let navigate = useNavigate()
+   
+   function registerSubmit(newUser){
+    
+      console.log(newUser)
+    }
   return (
-    <div>
-      Signup
+    <div className='m-6'>
+        <div className='text-center '>
+          <img className='m-auto w-40' src='https://png.pngtree.com/template/20190926/ourmid/pngtree-neuron-cell-biotech-nanotechnology-molecule-logo-vector-icon-image_309801.jpg'></img>
+        <h1 className='font-semibold text-2xl'> SHMR</h1>        
+        </div>
+
+        <div className=' border-black rounded-md my-4 border-2 w-96 m-auto '>
+            <h1 className='text-3xl font-semibold m-3 mx-4'>Create Account</h1>
+            <form className='px-10' onSubmit={handleSubmit(registerSubmit, ()=>{console.log('error')})}>
+            <div className='my-4'>
+            <h1 className='font-semibold'>Your Name</h1>
+            <input   placeholder='First and last name' className='w-full  shadow-sky-600 shadow-md border-gray-500 border-2  text-black p-1 rounded-sm' type='' {...register('Name', {required:true})}></input>
+            {errors.Name?.type ==='required' && (<h3 className='px-1 text-sm  text-red-600'>! Enter your name</h3>)}
+            </div>
+            <div className='my-4'>
+            <h1 className='font-semibold ' >Mobile Number</h1>
+            <div className='flex'>
+            <select className='w-20 mx-4 rounded-md shadow-md bg-slate-200'>
+                <option> IN +91</option>
+                <option>AB +355</option>
+                <option>AF +93</option>
+                <option>AL +213</option>
+                <option>AD +376</option>
+                <option>AU +61</option>
+            </select>
+            <input   placeholder='Mobile Number' className='border-gray-500 border-2  shadow-sky-600 shadow-md text-black p-1 rounded-sm' type='' {...register('Number', {required:true, maxLength:10,minLength:10})}></input>
+           
+            </div >  
+            <div>
+            {errors.Number?.type ==='required' && (<h3 className='px-1 text-sm  text-red-600'>! Enter your mobile number</h3>)}
+            {errors.Number?.type=== 'minLength' && (<h3 className='px-1 text-sm  text-red-600'>*required 10 numbers </h3>)}
+            {errors.Number?.type=== 'maxLength' && (<h3 className='px-1 text-sm  text-red-600'>*required 10 numbers </h3>)}
+            </div>
+           
+            </div>
+            <div className='my-4'>
+            <h1 className='font-semibold '>Email</h1>
+            <input   className='w-full border-gray-500 border-2 shadow-sky-600 shadow-md text-black p-1 rounded-sm' type='email' {...register('Email', {required:true, minLength:8})}></input>
+            {errors.Email?.type ==='required' && (<h3 className='px-1 text-sm  text-red-600'>! Enter your email</h3>)}
+            </div>
+            <div className='my-4'>
+            <h1 className='font-semibold m-2'>Password</h1>
+            <input  id='Pass' className='w-full border-gray-500 border-2 s shadow-sky-600 shadow-md text-black p-1 rounded-sm' type='password' {...register('Pass', {required:true, minLength:8})}></input>
+            {errors.Pass?.type ==='required' && (<h3 className='px-1 text-sm  text-red-600'>! Enter your password</h3>)}
+            {errors.Pass?.type=== 'minLength' && (<h3 className='px-1 text-sm  text-red-600'>Password must be atleat 8 cahracters</h3>)}
+            </div>
+          { /* <div className='my-4'>
+            <h1 className='font-semibold m-2 '> Re-Enter Password</h1>
+            <input  id='Pass' className='w-full border-gray-500 border-2  shadow-sky-600 shadow-md text-black p-1 rounded-sm' type='password' {...register('Pass', {required:true, minLength:8})}></input>
+            {errors.Pass?.type ==='required' && (<h3 className='px-1 text-sm  text-red-600'>! Enter your password</h3>)}
+            {errors.Pass?.type=== 'minLength' && (<h3 className='px-1 text-sm  text-red-600'>Password must be atleat 8 cahracters</h3>)}
+            </div>
+            <div>*/}
+                <div><p className='my-10'>To verify your number, we will send you a text message with a temporary code. Message and data rates may apply.</p>
+            </div>
+            <div className='text-center'>
+            <button type='submit' className=' bg-gray-600 text-lg rounded-xl p-3 text-white hover:bg-sky-600     '>Sign Up</button>
+            </div>
+            
+            </form>
+            <p className='text-center text-lg py-3'>Already Registered !
+              <NavLink to='/login' className='underline px-3 text-blue-700 font-semibold text-lg'>Login</NavLink>
+            </p>
+                    </div>
     </div>
   )
 }
 
-export default SignupPage
+export default Signup
